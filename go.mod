246a3df27fb9c99bbf27136329620ccfeb5a98ea
Nikifor <NikiforMalkov@gmail.com>
2021-07-18 16:18:11 +0600

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/NikiforMalkov/hw_19
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import "fmt"
+
+type segment struct {
+	sum   int
+	start int
+	end   int
+}
+
+func (s *segment) String() string {
+	return fmt.Sprintf("started at: %d, ended at: %d", s.start, s.end)
+}
+
+func main() {
+	data := []int{10, -3, -12, 8, 42, 1, -7, 0, 8, 42, 2, 5}
+	result := findClosedSubarrayWithMaxSum(data)
+	fmt.Println("Result ", result.String())
+}
+
+//findClosedSubarrayWithMaxSum если я правильно понял, тогда закрытым будет считаться элемент, который ограничен двумя другими, начало и конец не являются закрывающими.
+func findClosedSubarrayWithMaxSum(array []int) segment {
+	max := segment{
+		sum:   0,
+		start: 0,
+		end:   0,
+	}
+
+	currentSegment := segment{
+		sum:   0,
+		start: 0,
+		end:   0,
+	}
+
+	//Чисто технически, если наша сумма уменьшилась, тогда мы нашли конец отрезка
+	for key, value := range array {
+		sum := currentSegment.sum + value
+		if currentSegment.sum > sum {
+			currentSegment.end = key - 1
+			if currentSegment.sum > max.sum {
+				max = currentSegment
+			}
+			currentSegment = segment{
+				sum:   0,
+				start: key + 1,
+				end:   0,
+			}
+		}
+		currentSegment.sum = sum
+	}
+	return max
+}
+
+//findSubarrayWithMaxSum Первый пример
+func findSubarrayWithMaxSum(array []int) segment {
+	max := segment{
+		sum:   0,
+		start: 0,
+		end:   0,
+	}
+
+	currentSegment := segment{
+		sum:   0,
+		start: 0,
+		end:   0,
+	}
+
+	for key, value := range array {
+		if value < 0 {
+			if max.sum < currentSegment.sum {
+				max = currentSegment
+			}
+			currentSegment = segment{
+				sum:   0,
+				start: 0,
+				end:   0,
+			}
+			currentSegment.start = key + 1
+		} else {
+			currentSegment.sum += value
+			fmt.Println(key - 1)
+			currentSegment.end = key
+		}
+	}
+	if currentSegment.sum > max.sum {
+		currentSegment.end = len(array) - 1
+		max = currentSegment
+	}
+	return max
+}
